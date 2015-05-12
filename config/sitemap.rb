ac4a01e2c5cc4e46453975a3098f32134c3ca725
Frank Pinto <frank@ayalo.co>
2015-05-11 21:46:25 -0600

    Use file that works with rake tasks

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,21 @@
+require 'anemone'
+require 'sitemap_generator'
+
+host = 'http://ayalo.aho.gt'
+
+urls = []
+Anemone.crawl(host) do |anemone|
+  anemone.on_every_page do |page|
+    uri = URI.parse(page.url.to_s)
+    puts uri.path
+    urls << uri.path
+  end
+end
+
+puts urls.count
+
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://www.example.com"
+SitemapGenerator::Sitemap.default_host = host
 
 SitemapGenerator::Sitemap.create do
   # Put links creation logic here.
