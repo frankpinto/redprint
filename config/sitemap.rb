535368466393a85dd59e955a5d568f8de6fbf8f9
Frank Pinto <frank@ayalo.co>
2015-05-11 22:02:02 -0600

    Don't strip pagination and delete root

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,9 +8,14 @@ Anemone.crawl(host) do |anemone|
   anemone.on_every_page do |page|
     uri = URI.parse(page.url.to_s)
     # puts uri.path
-    urls << uri.path
+    urls << uri.path + '?' + uri.query.to_s
   end
 end
+
+# Clean up urls a bit
+urls.sort!
+urls.delete_at 0 # Delete root as SitemapGenerator adds it in
+urls.uniq!
 
 puts urls.count
 
