6c1d134dc56d98b5e2d19ad4105520c9b7868f47
Frank Pinto <frank@ayalo.co>
2015-05-11 21:48:44 -0600

    Don't be so verbose for now

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -7,7 +7,7 @@ urls = []
 Anemone.crawl(host) do |anemone|
   anemone.on_every_page do |page|
     uri = URI.parse(page.url.to_s)
-    puts uri.path
+    # puts uri.path
     urls << uri.path
   end
 end
@@ -18,26 +18,7 @@ puts urls.count
 SitemapGenerator::Sitemap.default_host = host
 
 SitemapGenerator::Sitemap.create do
-  # Put links creation logic here.
-  #
-  # The root path '/' and sitemap index file are added automatically for you.
-  # Links are added to the Sitemap in the order they are specified.
-  #
-  # Usage: add(path, options={})
-  #        (default options are used if you don't specify)
-  #
-  # Defaults: :priority => 0.5, :changefreq => 'weekly',
-  #           :lastmod => Time.now, :host => default_host
-  #
-  # Examples:
-  #
-  # Add '/articles'
-  #
-  #   add articles_path, :priority => 0.7, :changefreq => 'daily'
-  #
-  # Add all articles:
-  #
-  #   Article.find_each do |article|
-  #     add article_path(article), :lastmod => article.updated_at
-  #   end
+  urls.sort.each do |url|
+    add url, changefreq: 'daily'
+  end
 end
