503e5ced69fe89c0d0338d4999de412e4caf16fa
Frank Pinto <frank@ayalo.co>
2015-05-19 11:59:29 -0600

    Move AWS credentials to dotenv files

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.2.0'
 
+gem 'dotenv'
 gem 'sitemap_generator'
 gem 'anemone'
 gem "fog", "~> 1.3.1"
--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,12 +1,12 @@
+require 'dotenv'
 require 'anemone'
-require 'sitemap_generator'
-require 'carrierwave'
 
+Dotenv.load
 host = ARGV[1] || 'http://ayalo.aho.gt'
 
 urls = ['/', '/motos']
 Anemone.crawl(host) do |anemone|
-  return
+  break
   anemone.on_every_page do |page|
     uri = URI.parse(page.url.to_s)
     # puts uri.path
@@ -19,14 +19,16 @@ urls.sort!
 urls.delete_at 0 # Delete root as SitemapGenerator adds it in
 urls.uniq!
 
-puts urls.count
+print 'URL count: ', urls.count, "\n"
 
+# Credentials in .env files
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = host
-SitemapGenerator::Sitemap.sitemaps_host = 'http://tools.ayalo.co.s3.amazonaws.com/'
+SitemapGenerator::Sitemap.sitemaps_host = 'http://tools.ayalo.co.s3-website-us-east-1.amazonaws.com/'
 SitemapGenerator::Sitemap.public_path = 'public/'
 SitemapGenerator::Sitemap.filename = 'sitemap_gt'
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new
+SitemapGenerator::Sitemap.verbose = true
 
 SitemapGenerator::Sitemap.create do
   urls.sort.each do |url|
